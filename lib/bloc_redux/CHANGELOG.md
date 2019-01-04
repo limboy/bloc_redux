362eded8a1fb597ba6bf755f5753683e7e3ad630
limboy <limboy.me@gmail.com>
2019-01-04 22:29:01 +0800

    update description

--- lib/bloc_redux/CHANGELOG.md
+++ lib/bloc_redux/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.6.2
+
+- update package description
+
 ## 0.6.1
 
 - add an example file for dart pub
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:semi_redux/main.dart';
+import 'package:bloc_redux/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
